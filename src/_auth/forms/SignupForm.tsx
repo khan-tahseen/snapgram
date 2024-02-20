5688af59613a598aa11077046f806b131942bdf6
khan-tahseen <tasheenkhanit@gmail.com>
2024-02-21 00:23:06 +0530

    config and .env file added

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -112,8 +112,14 @@ const SignupForm = () => {
             )}
           </Button>
 
-          <p className='text-small-regular text-light-2 text-center mt-2'>
-            Already have an account? <Link className='text-primary-500 text-small-semibold ml-2' to="/sign-in">Sign in</Link>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?{' '}
+            <Link
+              className="text-primary-500 text-small-semibold ml-2"
+              to="/sign-in"
+            >
+              Sign in
+            </Link>
           </p>
         </form>
       </div>
---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,11 @@
+import { Account, Avatars, Client, Databases, Storage } from 'appwrite';
+
+export const appwriteConfig = {
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+};
+
+export const client = new Client();
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
